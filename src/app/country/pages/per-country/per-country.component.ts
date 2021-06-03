f269f8575e13131a69ee334ecc561d39828b6e88
sifloz <hecn@live.com.mx>
2021-06-03 18:41:13 -0500

    Added handler to search countries and display them

--- src/app/country/pages/per-country/per-country.component.ts
+++ src/app/country/pages/per-country/per-country.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/Country.interface';
 
 @Component({
   selector: 'app-per-country',
@@ -6,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PerCountryComponent implements OnInit {
+export class PerCountryComponent {
+  query: string = '';
+  hasError: boolean = false;
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) {}
 
-  ngOnInit(): void {
+  searchHandler() {
+    this.hasError = false;
+    this.countryService.searchCountry(this.query)
+      .subscribe((countries) => {
+        console.log(countries);
+        this.countries = countries;
+      }, (err) => {
+        this.hasError = true;
+        this.countries = [];
+      });
   }
-
 }
