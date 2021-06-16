8280d3509ca66834f41d0af70eff15f7261116eb
sifloz <hecn@live.com.mx>
2021-06-16 15:43:26 -0500

    Added methods and variables to search by capital city

--- src/app/country/pages/per-capital/per-capital.component.ts
+++ src/app/country/pages/per-capital/per-capital.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/Country.interface';
 
 @Component({
   selector: 'app-per-capital',
@@ -6,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PerCapitalComponent implements OnInit {
+export class PerCapitalComponent {
+  query: string = '';
+  hasError: boolean = false;
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) {}
 
-  ngOnInit(): void {
+  searchHandler(query: string) {
+    
+    this.hasError = false;
+    this.query = query;
+
+    this.countryService.searchCapital(this.query)
+      .subscribe((countries) => {
+        console.log(countries);
+        this.countries = countries;
+      }, (err) => {
+        this.hasError = true;
+        this.countries = [];
+      });
   }
-
 }
