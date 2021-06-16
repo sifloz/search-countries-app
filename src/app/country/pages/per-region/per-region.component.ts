c0ec5d66cf3942eb54046f67bfd0aa5984506130
sifloz <hecn@live.com.mx>
2021-06-16 18:02:08 -0500

    Added logic to call country service and display button class conditionally

--- src/app/country/pages/per-region/per-region.component.ts
+++ src/app/country/pages/per-region/per-region.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/Country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-per-region',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PerRegionComponent implements OnInit {
+export class PerRegionComponent {
 
-  constructor() { }
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
+  countries: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private countryService: CountryService) { }
+
+  getCSSClass(region: string): string {
+    return `p-4 border border-gray-300 rounded block w-full ${region === this.activeRegion ? 'bg-gray-100' : null}`;
+  }
+
+  activateRegionHandler(region: string) {
+
+    if(region === this.activeRegion) { return; }
+
+    this.activeRegion = region;
+    this.countries = [];
+
+    this.countryService.getCountriesByRegion(region)
+      .subscribe((countries) => {
+        this.countries = countries;
+      })
   }
 
 }
