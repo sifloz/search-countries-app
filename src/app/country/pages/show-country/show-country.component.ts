ea9802ba8cbe4ab816838a4a96b626b5a0a7c248
sifloz <hecn@live.com.mx>
2021-06-16 15:58:59 -0500

    Using switchMap to switch between observables while fetching country by alpha

--- src/app/country/pages/show-country/show-country.component.ts
+++ src/app/country/pages/show-country/show-country.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-show-country',
@@ -8,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowCountryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private countryService: CountryService) { }
 
   ngOnInit(): void {
+    // this.activatedRoute.params
+    //   .subscribe(({ countryId }) => {
+    //     this.countryService.getCountryByAlpha(countryId)
+    //       .subscribe(country => console.log(country))
+    //   });
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({countryId}) => this.countryService.getCountryByAlpha(countryId))
+      )
+      .subscribe(res => {
+        console.log(res)
+      })
   }
 
 }
