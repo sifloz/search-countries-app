e124130a6d6e0bb0d88a47d51842d37461ba39d8
sifloz <hecn@live.com.mx>
2021-06-16 18:20:16 -0500

    Added params to fetch only certain params in order to reduce response size

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Country } from '../interfaces/Country.interface';
@@ -10,17 +10,21 @@ import { Country } from '../interfaces/Country.interface';
 export class CountryService {
 
   private _apiUrl: string = 'https://restcountries.eu/rest/v2'
+  
+  get httpParams() {
+    return new HttpParams().set('fields', 'name;capital;alpha2Code;flag;population');
+  }
 
   constructor(private http: HttpClient) { }
 
   searchCountry(query: string): Observable<Country[]> {
     const url = `${this._apiUrl}/name/${query}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   searchCapital(query: string): Observable<Country[]> {
     const url = `${this._apiUrl}/capital/${query}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   getCountryByAlpha(id: string): Observable<Country> {
@@ -29,7 +33,8 @@ export class CountryService {
   }
 
   getCountriesByRegion(region: string): Observable<Country[]> {
+    
     const url = `${this._apiUrl}/region/${region}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 }
